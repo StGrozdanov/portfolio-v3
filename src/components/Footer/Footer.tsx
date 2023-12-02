f8ccbea29398d2ed5b3c3bf6da6a92499e339574
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2023-12-02 15:26:13 +0200

    use the actual Vercel deployment URL on production env

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,8 +4,14 @@ import Animate from '../Animate/Animate';
 import Script from 'next/script';
 
 export default async function Footer() {
+    const BASE_API_URL = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+        : process.env.BASE_API_URL;
+
     let socials: SocialMedia | null = null;
-    const response = await fetch(`${process.env.BASE_API_URL}/get-socials`);
+
+    const response = await fetch(`${BASE_API_URL}/get-socials`);
+    
     if (response.ok) socials = await response.json();
     return (
         <>
