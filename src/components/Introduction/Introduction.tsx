75437bd5c4a9a463155ea79268cdcc3eb885ade6
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2023-12-19 11:29:14 +0200

    use the external API for tracking

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -7,53 +7,26 @@ import Partners from './modules/components/Partners/Partners';
 import Resume from './modules/components/Resume/Resume';
 import CarouselLinks from './modules/components/CarouselLinks/CarouselLinks';
 import { useFetchBasicUserInfo } from './modules/hooks/useFetchBasicUserInfo';
-import { TrackingInfoResponse } from '@/app/api/track-visitor/route';
 import { useDeviceDetection } from '@/hooks/useDeviceDetection';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Introduction() {
     const { basicUserInfo, carouselData } = useFetchBasicUserInfo();
-    const [userLocationData, setUserLocationData] = useState<TrackingInfoResponse>();
     const visitaitonDevice = useDeviceDetection();
-
-    const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
-        : process.env.NEXT_PUBLIC_BASE_API_URL;
 
     useEffect(() => {
         if (visitaitonDevice) {
-            fetch(`${BASE_URL}/track-visitor`)
-                .then(data => data.json())
-                .then(response => {
-                    const locationData: TrackingInfoResponse = {
-                        country_code: response.country_code,
-                        country_name: response.country_name,
-                        city: response.city,
-                        postal: response.postal,
-                        IPv4: response.IPv4,
-                        state: response.state
-                    };
-                    setUserLocationData(locationData);
-                })
-                .catch(err => console.error(err));
-        }
-    }, [visitaitonDevice]);
-
-    useEffect(() => {
-        if (visitaitonDevice && userLocationData) {
-            fetch(`${BASE_URL}/analytics`, {
+            fetch(`${process.env.NEXT_PUBLIC_TRACKING_API}/analytics/track`, {
                 method: 'POST',
                 body: JSON.stringify({
                     deviceType: visitaitonDevice,
-                    ipAddress: userLocationData.IPv4,
-                    originCountry: userLocationData.country_name,
                 }),
                 headers: new Headers({
                     'Content-Type': 'application/json',
                 }),
             }).catch(err => console.error(err));
         }
-    }, [visitaitonDevice, userLocationData]);
+    }, [visitaitonDevice]);
 
     return (
         <>
