1fce9bd768d2dbe4919fc9ca4a9db7ba0a17c655
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2024-02-04 09:17:14 +0200

    fix failing integration tests

--- src/tests/mocks/data.mock.ts
+++ src/tests/mocks/data.mock.ts
@@ -1,6 +1,6 @@
 export const mockSocialsResponse = {
-    facebook: 'https://www.facebook.com/example',
-    linkedIn: 'https://www.linkedin.com/in/example',
-    github: 'https://www.github.com/example',
-    email: 'example@example.com',
+    facebook: '#',
+    linkedIn: 'https://www.linkedin.com/in/stoyan-grozdanov/',
+    github: 'https://github.com/StGrozdanov',
+    email: 'st.grozdanov.developer@gmail.com',
   };
