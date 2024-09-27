a9c3697ca2bc166248f5eb61fa380d75d98e644f
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2024-09-27 18:24:15 +0300

    oops actual method for monitoring needs to be HEAD

--- src/app/api/monitoring/route.ts
+++ src/app/api/monitoring/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 /**
  * Handler being used from uptime monitor in order to prevent supabase from pausing the database 
  */
-export async function GET() {
+export async function HEAD() {
   return NextResponse.json({ status: "ok" }, { status: 200 });
 }
